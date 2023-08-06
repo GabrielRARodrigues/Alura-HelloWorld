2795c3618de4911a0276242666103c0ac83427a8
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-08-06 14:37:58 -0300

    Modifying the Post component and creating the ScrollToTop component
    Making the final modifications to the project's structure, by modifying the Post component and adding a new section to display recommended and related posts based on what the user is currently reading. Next, implementing a new logic to create a list of Posts using array methods like filter, sort, and slice for this task. Finally, creating the ScrollToTop component to ensure that the page scrolls to the top when the user changes routes

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,4 +1,5 @@
 import './Post.css'
+import styles from './Post.module.css'
 
 import { Route, Routes, useParams } from 'react-router-dom'
 import posts from 'json/posts.json'
@@ -6,6 +7,7 @@ import PostModelo from 'components/PostModelo'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import NaoEncontrada from 'pages/NaoEncontrada'
 import PaginaPadrao from 'components/PaginaPadrao'
+import PostCard from 'components/PostCard'
 
 export default function Post() {
   const { id } = useParams()
@@ -15,6 +17,11 @@ export default function Post() {
   if (!post) {
     return <NaoEncontrada />
   }
+
+  const postsRecomendados = posts
+    .filter(post => post.id !== Number(id))
+    .sort((a, b) => b.id - a.id)
+    .slice(0, 4)
 
   return (
     <Routes>
@@ -29,6 +36,16 @@ export default function Post() {
               <div className="post-markdown-container">
                 <ReactMarkdown>{post.texto}</ReactMarkdown>
               </div>
+              <h2 className={styles.tituloOutrosPosts}>
+                Outros posts que você pode gostar:
+              </h2>
+              <ul className={styles.postsRecomendados}>
+                {postsRecomendados.map(post => (
+                  <li key={post.id}>
+                    <PostCard post={post} />
+                  </li>
+                ))}
+              </ul>
             </PostModelo>
           }
         />
